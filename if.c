2737c82c89b293f291b55a3909417b02a6d950a9
kangminseokk <ejdsha050216@gmail.com>
2024-09-01 21:08:24 +0900

    add if

---
+++ if.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int score = 85; //학생의 점수르 의미합니다
+	if(score>=90)
+	{
+		printf("당신의 학점은A입니다.\n");
+	
+	 } 
+	else if(score >=80)
+	{
+		printf("당신의 학점은 B입니다.\n");
+	}
+	else if("score>=70")
+	{
+		printf("당신의 학점점은C입니다.\n");
+	}
+	else
+	{
+		printf("당신의 학점은F입니다,\n");
+	}
+	return 0;  
+}
